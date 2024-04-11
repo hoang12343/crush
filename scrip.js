7fd0075051ee3c766e17ff1d31069560dcecdaf3
hoang12343 <hoanggg322004@gmail.com>
2024-04-11 21:59:15 +0700

    gui code

---
+++ scrip.js
@@ -0,0 +1,29 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+const questionPrompt = document.querySelector(".question-prompt"); // Thêm đoạn này
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "QUÁ KHEN RỒI :)<br>iu x10000000";
+  gif.src =
+    "https://media.giphy.com/media/vvc1dJPLEU2QOw9cXy/giphy.gif";
+
+  // Ẩn cả hai nút Yes và No
+  yesBtn.style.display = "none";
+  noBtn.style.display = "none";
+  questionPrompt.style.display = "none"; // Ẩn dòng chữ khi người dùng nhấn vào nút Yes
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
